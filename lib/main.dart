20ad6ab72c6575d5a0ff4c442783de333c40f2e7
Nadhif <nadhif@Nadhifs-Family-Macbook.local>
2023-03-14 13:21:29 +0700

    Styling & Theming

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        fontFamily: 'Lato',
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+            .copyWith(secondary: Colors.deepOrange),
+      ),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
