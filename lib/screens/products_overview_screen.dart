244717c49f9c413a755e5a3914be23f840d2745b
if_alan <nadhif@Nadhifs-Family-Macbook.local>
2023-03-15 10:54:16 +0700

    Favorite List Content

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,12 +2,39 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favorites) {
+                // ..
+              } else {
+                // ..
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+            icon: Icon(Icons.more_vert),
+          )
+        ],
       ),
       body: ProductsGrid(),
     );
